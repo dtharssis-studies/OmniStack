c8b5c1fe651631a2e04e2fabaa61c45f816ea298
Danilo Tharssis Araújo <dtharssis@gmail.com>
2020-03-26 23:03:04 -0300

    Backe-end-application

---
+++ backend/docs.js
@@ -0,0 +1,31 @@
+/**
+ * Método HTTP:
+ * 
+ * GET: Buscar informaçõa no Backend
+ * POST: Criar uma informação no backend
+ * PUT: Alterar um informação no Backend
+ * DELETE: Deletar uma informação no Backend
+ * 
+ * Tipos de parâmetros
+ */
+
+/** 
+ * Query Parans: Parâmetros nomeados enviados na rota após '?' (Filtros, paginação)
+ * Route Parans: Parâmetros utilizados para identidicar recurso
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+ * 
+ * /
+
+ /**
+  * SQL: MySql, SQLite, PostgresSQL, Oracle, Microsoft SQL Server
+  * NoSql: MongoDB, etc
+ */
+
+ /**
+  * DRIVER: SELECT * FROM tablename;
+  * Query  Builder: table('users).select('*').where()
+  */
+
+// const params = request.query;
+// const params = request.params;
+// const body = request.body;
